363d267bd8a01e451f3063b048da57688437d5f9
Alex Kondrachenko <aleksej.kondrachenko@4px.ru>
2022-08-03 16:29:54 +0300

    Создание переменных

--- app 1/Program.cs
+++ app 1/Program.cs
@@ -6,7 +6,16 @@ namespace app_1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("ЯЮниор");
+            string firtsName = "Alex";
+            string lastName = "Kondrachenko";
+            string county = "Belarus";
+            int age = 28;
+            int year = 2022;
+            int month = 08;
+            int day = 03;
+            double numberPi = 3.14;
+            bool positiveNumber = true;
+            bool negativeNumber = false;
         }
     }
 }
