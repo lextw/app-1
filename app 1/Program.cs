0a1a3dbf7f5e4084d72f2cf689da8b129142a901
Alex Kondrachenko <aleksej.kondrachenko@4px.ru>
2022-08-04 11:44:24 +0300

    Изменены имена переменных

--- app 1/Program.cs
+++ app 1/Program.cs
@@ -10,12 +10,12 @@ namespace app_1
             int age = 28;
             bool isWork = true;
             double numberPi = 3.14;
-            float f = 3.14f;
+            float distance = 3.14f;
             decimal num = 11.04m;
             char firstLetter = 'a';
-            var a = 1.4;
-            byte b = 2;
-            sbyte c = 4;
+            var distanceToFinish = 1.4;
+            byte members = 2;
+            sbyte numberOfPlayers = 4;
 
         }
     }
